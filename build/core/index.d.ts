374ded02e9b6c7b1f92b943293d2d628c8553464
Emil Widlund <ewidlund@ea.com>
2019-08-16 00:28:49 +0200

    Remove build

--- build/core/index.d.ts
+++
@@ -1,5 +0,0 @@
-export * from './Context';
-export * from './Connection';
-export * from './Node';
-export * from './Port';
-export * from './nodes';
--- build/helpers.d.ts
+++
@@ -1,4 +0,0 @@
-export declare type UnkownObject = {
-    [key: string]: any;
-};
-export declare const serializeObject: (object: UnkownObject) => UnkownObject;
