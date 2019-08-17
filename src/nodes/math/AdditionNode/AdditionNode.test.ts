e352dca57720bcb94feb472a2b953701e7d64a4d
Emil Widlund <ewidlund@ea.com>
2019-08-17 16:16:30 +0200

    Add tests for Addition & Subtraction Nodes

--- src/nodes/math/AdditionNode/AdditionNode.test.ts
+++ src/nodes/math/AdditionNode/AdditionNode.test.ts
@@ -1,1 +1,15 @@
-test.todo('Write test for Addition Node');
+import { Context } from '../../../core';
+import { AdditionNode } from './AdditionNode';
+
+describe('Addition Node', () => {
+    test('Should compute values correctly', () => {
+        const ctx: Context = new Context();
+
+        const nodeA = new AdditionNode(ctx);
+
+        nodeA.inputPorts.a.value = 100;
+        nodeA.inputPorts.b.value = 100;
+
+        expect(nodeA.outputPorts.result.value).toBe(200);
+    });
+});
--- src/nodes/math/SubtractionNode/SubtractionNode.test.ts
+++ src/nodes/math/SubtractionNode/SubtractionNode.test.ts
@@ -1,1 +1,15 @@
-test.todo('Write test for Subtraction Node');
+import { Context } from '../../../core';
+import { SubtractionNode } from './SubtractionNode';
+
+describe('Subtraction Node', () => {
+    test('Should compute values correctly', () => {
+        const ctx: Context = new Context();
+
+        const nodeA = new SubtractionNode(ctx);
+
+        nodeA.inputPorts.a.value = 100;
+        nodeA.inputPorts.b.value = 50;
+
+        expect(nodeA.outputPorts.result.value).toBe(50);
+    });
+});
